7313883e15ab02528dfcdd53b69c68fd25031837
ouyang <jxfengzi@gmail.com>
2019-05-06 18:22:25 +0800

    update libs

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {Convert} from 'mipher';
 
 console.log('hello');
 
-const deviceId = '10001@10006';
+const deviceId = '10001';
 const productId = 10006;
 const productVersion = 1;
 const k = '/8meBcfecxNl7pMIO0Zxbhx70A4DSGio7C2H7VzZLB8=';
@@ -16,8 +16,8 @@ const cipher = new XcpClientCipherProductImpl(productId, productVersion, getter,
 const codec = XcpFrameCodecType.NOT_CRYPT;
 const client = new XcpClientImpl(deviceId, productId, productVersion, cipher, codec);
 
-const host = '127.0.0.1';
-const port = 9090;
+const host = 'accesspoint.geekool.cn';
+const port = 80;
 const uri = '/endpoint';
 client.connect(host, port, uri).then(x => {
     console.log('connect to xcp server ok!');
